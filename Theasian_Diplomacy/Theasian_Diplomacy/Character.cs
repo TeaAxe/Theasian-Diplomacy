c532c97ba29f9f7bb4251bd201ea431214269917
sathalom <samuel.theriault.hall@gmail.com>
2015-06-18 22:23:24 -0400

    Added basic Faction and Character classes

---
+++ Theasian_Diplomacy/Theasian_Diplomacy/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theasian_Diplomacy
+{
+    class Character
+    {
+        private string firstName;
+        //No need for last names, it comes from faction
+
+        Faction faction;
+
+        public void Character(string _firstName, Faction _faction)
+        {
+            firstName = _firstName;
+            faction = _faction;
+        }
+
+
+    }
+}
---
+++ Theasian_Diplomacy/Theasian_Diplomacy/Faction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theasian_Diplomacy
+{
+    class Faction
+    {
+
+        private string factionName;
+        List<Character> members;
+
+        public void Faction(string _factionName)
+        {
+            factionName = _factionName;
+            members = new List<Character>();
+        }
+
+
+    }
+}
