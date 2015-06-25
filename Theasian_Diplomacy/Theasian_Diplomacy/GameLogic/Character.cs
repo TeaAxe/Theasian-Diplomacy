9da4ae5b1c9674c5135127f2f53805a4df93a725
sathalom <samuel.theriault.hall@gmail.com>
2015-06-25 19:55:56 -0400

    Moved GameLogic fiels to GameLogic folder

---
+++ Theasian_Diplomacy/Theasian_Diplomacy/GameLogic/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theasian_Diplomacy
+{
+    class Character
+    {
+        private string firstName;
+        //No need for last names, it comes from faction
+
+        Faction faction;
+
+        public Character(string _firstName, Faction _faction)
+        {
+            firstName = _firstName;
+            faction = _faction;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + faction.ToString();
+        }
+
+
+    }
+}
