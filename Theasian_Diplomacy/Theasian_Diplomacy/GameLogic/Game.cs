9419fd3a3f251f3da6caa38eefe5e6d262a0d79b
jeepee12 <jeepee.12@hotmail.com>
2015-08-11 14:48:21 -0400

    Prep à l'intégration des context pour faire le lien avec la BD

--- Theasian_Diplomacy/Theasian_Diplomacy/GameLogic/Game.cs
+++ Theasian_Diplomacy/Theasian_Diplomacy/GameLogic/Game.cs
@@ -29,40 +29,58 @@ namespace Theasian_Diplomacy.GameLogic
             factions = new List<Faction>();
             cities = new List<City>();
             locations = new List<Location>();
+            using (TheasianDiplomacyDBEntities db = new TheasianDiplomacyDBEntities())
+            {
 
-            //Adds factions to the list, needs to come from a list
-            Faction newFaction = new Faction("Barbarians");
-            newFaction.addMember(new Character("John", newFaction, new DateTime()), true);
-            Location newLocation = new Location("La ville des Chats", "Il y a beaucoup de chats ici!");
-            City newCity = new City("La ville des Chats", 100, newLocation);
-            newCity.Owner = newFaction;
-            cities.Add(newCity);
-            factions.Add(newFaction);
+                //Adds factions to the list, needs to come from a list
+                FactionDB factionDb = new FactionDB();
+                factionDb.name = "Barbarians";
+                factionDb.gold = 0;
+                factionDb.fFactionGoalID = 1;//mettre vrai ID
+                factionDb.fCharacterDBID = 1;//mettre vrai ID
 
-            newFaction = new Faction("Player_House");
-            newFaction.addMember(new Character("Generic", newFaction, new DateTime()), true);
-            player = newFaction.Members.ElementAt(0);
-            newLocation = new Location("La ville des Souris", "Il y a beaucoup de souris ici!");
-            newCity = new City("La ville des Souris", 100, newLocation);
-            newCity.Owner = newFaction;
-            cities.Add(newCity);
-            factions.Add(newFaction);
+                db.FactionDB.Add(factionDb);
+                db.SaveChanges();
 
-            newFaction = new Faction("People");
-            newFaction.addMember(new Character("Yolo", newFaction, new DateTime()), true);
-            newLocation = new Location("La ville des Marmottes", "Il y a beaucoup de marmottes ici!");
-            newCity = new City("La ville des Marmottes", 100, newLocation);
-            newCity.Owner = newFaction;
-            cities.Add(newCity);
-            factions.Add(newFaction);
+                Faction newFaction = new Faction("Barbarians");
+                CharacterDB characterDB = new CharacterDB();
+                characterDB.firstName = "John";
+                characterDB.honor = 2;
+                characterDB.birthDate = new DateTime(1995, 5, 3);
+                characterDB.cArmyDBID = 1;//mettre vrai ID
 
-            newFaction = new Faction("Foreign_Nation");
-            newFaction.addMember(new Character("swEG", newFaction, new DateTime()), true);
-            newLocation = new Location("La ville des Chiens", "Il y a beaucoup de chiens ici!");
-            newCity = new City("La ville des Chiens", 100, newLocation);
-            newCity.Owner = newFaction;
-            cities.Add(newCity);
-            factions.Add(newFaction);
+                newFaction.addMember(new Character("John", newFaction, new DateTime()), true);
+                Location newLocation = new Location("La ville des Chats", "Il y a beaucoup de chats ici!");
+                City newCity = new City("La ville des Chats", 100, newLocation);
+                newCity.Owner = newFaction;
+                cities.Add(newCity);
+                factions.Add(newFaction);
+
+                newFaction = new Faction("Player_House");
+                newFaction.addMember(new Character("Generic", newFaction, new DateTime()), true);
+                player = newFaction.Members.ElementAt(0);
+                newLocation = new Location("La ville des Souris", "Il y a beaucoup de souris ici!");
+                newCity = new City("La ville des Souris", 100, newLocation);
+                newCity.Owner = newFaction;
+                cities.Add(newCity);
+                factions.Add(newFaction);
+
+                newFaction = new Faction("People");
+                newFaction.addMember(new Character("Yolo", newFaction, new DateTime()), true);
+                newLocation = new Location("La ville des Marmottes", "Il y a beaucoup de marmottes ici!");
+                newCity = new City("La ville des Marmottes", 100, newLocation);
+                newCity.Owner = newFaction;
+                cities.Add(newCity);
+                factions.Add(newFaction);
+
+                newFaction = new Faction("Foreign_Nation");
+                newFaction.addMember(new Character("swEG", newFaction, new DateTime()), true);
+                newLocation = new Location("La ville des Chiens", "Il y a beaucoup de chiens ici!");
+                newCity = new City("La ville des Chiens", 100, newLocation);
+                newCity.Owner = newFaction;
+                cities.Add(newCity);
+                factions.Add(newFaction);
+            }
         }
 
         /// <summary>
