17fe03d1bc86e7fb93da74644cfee58eed2358eb
jeepee12 <jeepee.12@hotmail.com>
2015-08-19 14:54:31 -0400

    Fix again the god damn database...

--- Theasian_Diplomacy/Theasian_Diplomacy/GameLogic/Game.cs
+++ Theasian_Diplomacy/Theasian_Diplomacy/GameLogic/Game.cs
@@ -33,80 +33,85 @@ namespace Theasian_Diplomacy.GameLogic
             //using (TheasianDiplomacyDBEntities db = new TheasianDiplomacyDBEntities())
             //{ 
             TheasianDiplomacyDBEntities db = new TheasianDiplomacyDBEntities();
-            
-                GoalDB goalDB = new GoalDB();
-                goalDB.description = "First Goal";
-                goalDB.name = "first";
 
-                db.GoalDB.Add(goalDB);
-                db.SaveChanges();
+            SkillDB test = new SkillDB();
+            test.name = "qwer";
+            db.SkillDB.Add(test);
+            db.SaveChanges();
 
-                //FR_connexion FRBD = (from FR in ctxFR.Fr_Connexion select FR).FirstOrDefault();
-                GoalDB testgoal = (from goal in db.GoalDB select goal).FirstOrDefault();
-                testgoal.name = "";
-                db.SaveChanges();
+            GoalDB goalDB = new GoalDB();
+            goalDB.description = "First Goal";
+            goalDB.name = "first";
 
-                //FactionGoalDB factionGoalDB = new FactionGoalDB();
-                //factionGoalDB.code = "penis";
-                //factionGoalDB.description = "first penis goal";
-                //factionGoalDB.name = "firstpenis";
-                //db.FactionGoalDB.Add(factionGoalDB);
-                //db.SaveChanges();
+            db.GoalDB.Add(goalDB);
+            db.SaveChanges();
 
-                //CharacterDB characterDB = new CharacterDB();
-                //characterDB.firstName = "John";
-                //characterDB.honor = 2;
-                //characterDB.birthDate = new DateTime(1995, 5, 3);
-                //characterDB.cGoalDBID = goalDB.GoalDBID;
-                //db.CharacterDB.Add(characterDB);
+            //FR_connexion FRBD = (from FR in ctxFR.Fr_Connexion select FR).FirstOrDefault();
+            GoalDB testgoal = (from goal in db.GoalDB select goal).FirstOrDefault();
+            testgoal.name = "";
+            db.SaveChanges();
 
-                ////Adds factions to the list, needs to come from a list
-                //FactionDB factionDb = new FactionDB();
-                //factionDb.name = "Barbarians";
-                //factionDb.gold = 0;
-                //factionDb.fFactionGoalID = factionGoalDB.FactionGoalID;//mettre vrai ID
-                //factionDb.fCharacterDBID = characterDB.CharacterDBID;//mettre vrai ID
-                //characterDB.cmemberFactionDBID = factionDb.FactionDBID;
-                
-                //db.FactionDB.Add(factionDb);
+            //FactionGoalDB factionGoalDB = new FactionGoalDB();
+            //factionGoalDB.code = "penis";
+            //factionGoalDB.description = "first penis goal";
+            //factionGoalDB.name = "firstpenis";
+            //db.FactionGoalDB.Add(factionGoalDB);
+            //db.SaveChanges();
 
-                //db.SaveChanges();
+            //CharacterDB characterDB = new CharacterDB();
+            //characterDB.firstName = "John";
+            //characterDB.honor = 2;
+            //characterDB.birthDate = new DateTime(1995, 5, 3);
+            //characterDB.cGoalDBID = goalDB.GoalDBID;
+            //db.CharacterDB.Add(characterDB);
 
-                Faction newFaction = new Faction("Barbarians");
-                
+            ////Adds factions to the list, needs to come from a list
+            //FactionDB factionDb = new FactionDB();
+            //factionDb.name = "Barbarians";
+            //factionDb.gold = 0;
+            //factionDb.fFactionGoalID = factionGoalDB.FactionGoalID;//mettre vrai ID
+            //factionDb.fCharacterDBID = characterDB.CharacterDBID;//mettre vrai ID
+            //characterDB.cmemberFactionDBID = factionDb.FactionDBID;
+
+            //db.FactionDB.Add(factionDb);
+
+            //db.SaveChanges();
+
+            Faction newFaction = new Faction("Barbarians");
 
 
-                newFaction.addMember(new Character("John", newFaction, new DateTime()), true);
-                Location newLocation = new Location("La ville des Chats", "Il y a beaucoup de chats ici!");
-                City newCity = new City("La ville des Chats", 100, newLocation);
-                newCity.Owner = newFaction;
-                cities.Add(newCity);
-                factions.Add(newFaction);
 
-                newFaction = new Faction("Player_House");
-                newFaction.addMember(new Character("Generic", newFaction, new DateTime()), true);
-                player = newFaction.Members.ElementAt(0);
-                newLocation = new Location("La ville des Souris", "Il y a beaucoup de souris ici!");
-                newCity = new City("La ville des Souris", 100, newLocation);
-                newCity.Owner = newFaction;
-                cities.Add(newCity);
-                factions.Add(newFaction);
+            newFaction.addMember(new Character("John", newFaction, new DateTime()), true);
+            Location newLocation = new Location("La ville des Chats", "Il y a beaucoup de chats ici!");
+            City newCity = new City("La ville des Chats", 100, newLocation);
+            newCity.Owner = newFaction;
+            cities.Add(newCity);
+            factions.Add(newFaction);
 
-                newFaction = new Faction("People");
-                newFaction.addMember(new Character("Yolo", newFaction, new DateTime()), true);
-                newLocation = new Location("La ville des Marmottes", "Il y a beaucoup de marmottes ici!");
-                newCity = new City("La ville des Marmottes", 100, newLocation);
-                newCity.Owner = newFaction;
-                cities.Add(newCity);
-                factions.Add(newFaction);
+            newFaction = new Faction("Player_House");
+            newFaction.addMember(new Character("Generic", newFaction, new DateTime()), true);
+            player = newFaction.Members.ElementAt(0);
+            newLocation = new Location("La ville des Souris", "Il y a beaucoup de souris ici!");
+            newCity = new City("La ville des Souris", 100, newLocation);
+            newCity.Owner = newFaction;
+            cities.Add(newCity);
+            factions.Add(newFaction);
 
-                newFaction = new Faction("Foreign_Nation");
-                newFaction.addMember(new Character("swEG", newFaction, new DateTime()), true);
-                newLocation = new Location("La ville des Chiens", "Il y a beaucoup de chiens ici!");
-                newCity = new City("La ville des Chiens", 100, newLocation);
-                newCity.Owner = newFaction;
-                cities.Add(newCity);
-                factions.Add(newFaction);
+            newFaction = new Faction("People");
+            newFaction.addMember(new Character("Yolo", newFaction, new DateTime()), true);
+            newLocation = new Location("La ville des Marmottes", "Il y a beaucoup de marmottes ici!");
+            newCity = new City("La ville des Marmottes", 100, newLocation);
+            newCity.Owner = newFaction;
+            cities.Add(newCity);
+            factions.Add(newFaction);
+
+            newFaction = new Faction("Foreign_Nation");
+            newFaction.addMember(new Character("swEG", newFaction, new DateTime()), true);
+            newLocation = new Location("La ville des Chiens", "Il y a beaucoup de chiens ici!");
+            newCity = new City("La ville des Chiens", 100, newLocation);
+            newCity.Owner = newFaction;
+            cities.Add(newCity);
+            factions.Add(newFaction);
             //}
         }
 
@@ -125,7 +130,7 @@ namespace Theasian_Diplomacy.GameLogic
         {
             foreach (City city in cities)
             {
-                    city.Owner.Gold += city.Income;
+                city.Owner.Gold += city.Income;
             }
 
             //move units? or do they have move points and move when you click? maybe a mix with Civ system?
